b7de8df0eecaf71b6ba2c4e9099ca176d61eef3b
nacchogonza <nachomgonzalez93@gmail.com>
2025-05-24 18:36:00 -0300

    Ajuste error en archivo dal.py

--- api/core/dal.py
+++ api/core/dal.py
@@ -1,14 +1,15 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
-from api import cliente
+from api.cliente.schemas import ClienteCreateRequest
+from api.cliente.models import Cliente
 from api import location
 
 async def obtener_clientes(db: AsyncSession):
-    result = await db.execute(select(cliente.models.Cliente).options(selectinload(cliente.models.Cliente.ciudad)))
+    result = await db.execute(select(Cliente).options(selectinload(Cliente.ciudad)))
     return result.scalars().all()
 
-async def crear_cliente(db: AsyncSession, cliente: cliente.schemas.ClienteCreateRequest):
+async def crear_cliente(db: AsyncSession, cliente: ClienteCreateRequest):
     nuevo_cliente = cliente.models.Cliente(**cliente)
     db.add(nuevo_cliente)
     await db.commit()
