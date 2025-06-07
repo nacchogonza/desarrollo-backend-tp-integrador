b51fa44116cf356841de8bc02c831defd494f3c5
nacchogonza <nachomgonzalez93@gmail.com>
2025-06-07 13:47:18 -0300

    Se agrega funcionalidad de autenticacion JWT (usuarios dummy)

--- api/cliente/endpoint.py
+++ api/cliente/endpoint.py
@@ -3,6 +3,8 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api.core import dal, database
 from api.core.schemas import ClienteResponse, ClienteCreateRequest
 
+from ..auth import get_current_active_user, User
+
 router = APIRouter()
 
 async def get_db():
@@ -10,7 +12,7 @@ async def get_db():
         yield session
 
 @router.get("/", response_model = list[ClienteResponse])
-async def listar(db: AsyncSession = Depends(get_db)):
+async def listar(db: AsyncSession = Depends(get_db), current_user: User = Depends(get_current_active_user)):
     return await dal.obtener_clientes(db)
 
 @router.post("/", response_model = ClienteResponse)
