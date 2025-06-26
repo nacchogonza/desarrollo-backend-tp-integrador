94434f5c6223d87ee1cdd11d9e093d028a3dc804
nacchogonza <nachomgonzalez93@gmail.com>
2025-06-26 01:07:42 -0300

    Se ajustan DAL (Get ALL y Create) de remitos faltantes

--- api/resources/remito_devolucion/dal.py
+++ api/resources/remito_devolucion/dal.py
@@ -4,42 +4,101 @@ from sqlalchemy.orm import selectinload
 from .schemas import (
     RemitoDevolucionCreateRequest,
 )
-from api.core.models import RemitoDevolucion
+from api.core.models import (
+    RemitoDevolucion,
+    Cliente,
+    Ciudad,
+    Provincia,
+    Producto,
+    Proveedor,
+    Sucursal,
+)
 
-async def create_remito_devolucion(db: AsyncSession, remito_data: RemitoDevolucionCreateRequest):
-    
-    db_remito = RemitoDevolucion(
-        fecha=remito_data.fecha,
-        cantidad=remito_data.cantidad,
-        id_cliente=remito_data.id_cliente,
-        id_producto=remito_data.id_producto,
-        id_sucursal=remito_data.id_sucursal
-    )
+
+async def create_remito_devolucion(
+    db: AsyncSession, remito_data: RemitoDevolucionCreateRequest
+):
+
+    db_remito = RemitoDevolucion(**remito_data.model_dump())
     db.add(db_remito)
     await db.commit()
     await db.refresh(db_remito)
-    return db_remito
+
+    result = await db.execute(
+        select(RemitoDevolucion)
+        .options(
+            selectinload(RemitoDevolucion.cliente)
+            .selectinload(Cliente.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoDevolucion.producto)
+            .selectinload(Producto.proveedor)
+            .selectinload(Proveedor.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoDevolucion.sucursal)
+            .selectinload(Sucursal.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .where(RemitoDevolucion.id == db_remito.id)
+    )
+
+    remito_devolucion_con_relacion = result.scalar_one()
+    return remito_devolucion_con_relacion
+
 
 async def get_remito_devolucion_by_id(db: AsyncSession, remito_id: int):
-    
+
     stmt = select(RemitoDevolucion).where(RemitoDevolucion.id == remito_id)
     result = await db.execute(stmt)
-    return result.scalar_one_or_none() # Obtiene un único resultado o None
+    return result.scalar_one_or_none()  # Obtiene un único resultado o None
+
 
 async def get_all_remitos_devolucion(db: AsyncSession, skip: int = 0, limit: int = 100):
-    
-    stmt = select(RemitoDevolucion).offset(skip).limit(limit)
+
+    stmt = (
+        select(RemitoDevolucion)
+        .options(
+            selectinload(RemitoDevolucion.cliente)
+            .selectinload(Cliente.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoDevolucion.producto)
+            .selectinload(Producto.proveedor)
+            .selectinload(Proveedor.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoDevolucion.sucursal)
+            .selectinload(Sucursal.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .offset(skip)
+        .limit(limit)
+    )
     result = await db.execute(stmt)
     return result.scalars().all()
 
-async def update_remito_devolucion(db: AsyncSession, remito_id: int, remito_update: RemitoDevolucionCreateRequest):
-   
+
+async def update_remito_devolucion(
+    db: AsyncSession, remito_id: int, remito_update: RemitoDevolucionCreateRequest
+):
+
     stmt = select(RemitoDevolucion).where(RemitoDevolucion.id == remito_id)
     result = await db.execute(stmt)
     db_remito = result.scalar_one_or_none()
-    
+
     if db_remito:
-        
+
         for key, value in remito_update.model_dump(exclude_unset=True).items():
             setattr(db_remito, key, value)
         await db.commit()
@@ -47,12 +106,13 @@ async def update_remito_devolucion(db: AsyncSession, remito_id: int, remito_upda
         return db_remito
     return None
 
+
 async def delete_remito_devolucion(db: AsyncSession, remito_id: int):
-    
+
     stmt = select(RemitoDevolucion).where(RemitoDevolucion.id == remito_id)
     result = await db.execute(stmt)
     db_remito = result.scalar_one_or_none()
-    
+
     if db_remito:
         await db.delete(db_remito)
         await db.commit()
--- api/resources/remito_transferencia/dal.py
+++ api/resources/remito_transferencia/dal.py
@@ -4,43 +4,103 @@ from sqlalchemy.orm import selectinload
 from .schemas import (
     RemitoTransferenciaCreateRequest,
 )
-from api.core.models import RemitoTransferencia
+from api.core.models import (
+    RemitoTransferencia,
+    Cliente,
+    Ciudad,
+    Provincia,
+    Producto,
+    Proveedor,
+    Sucursal,
+)
 
 
-async def create_remito_transferencia(db: AsyncSession, remito_data: RemitoTransferenciaCreateRequest):
-    
-    db_remito = RemitoTransferencia(
-        fecha=remito_data.fecha,
-        cantidad=remito_data.cantidad,
-        id_sucursal=remito_data.id_sucursal,
-        id_producto=remito_data.id_producto,
-        id_deposito=remito_data.id_deposito
-    )
+async def create_remito_transferencia(
+    db: AsyncSession, remito_data: RemitoTransferenciaCreateRequest
+):
+
+    db_remito = RemitoTransferencia(**remito_data.model_dump())
     db.add(db_remito)
     await db.commit()
-    await db.refresh(db_remito) 
-    return db_remito
+    await db.refresh(db_remito)
+
+    result = await db.execute(
+        select(RemitoTransferencia)
+        .options(
+            selectinload(RemitoTransferencia.cliente)
+            .selectinload(Cliente.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoTransferencia.producto)
+            .selectinload(Producto.proveedor)
+            .selectinload(Proveedor.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoTransferencia.sucursal)
+            .selectinload(Sucursal.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .where(RemitoTransferencia.id == db_remito.id)
+    )
+
+    remito_transferencia_con_relacion = result.scalar_one()
+    return remito_transferencia_con_relacion
+
 
 async def get_remito_transferencia_by_id(db: AsyncSession, remito_id: int):
-    
+
     stmt = select(RemitoTransferencia).where(RemitoTransferencia.id == remito_id)
     result = await db.execute(stmt)
     return result.scalar_one_or_none()
 
-async def get_all_remitos_transferencia(db: AsyncSession, skip: int = 0, limit: int = 100):
-    
-    stmt = select(RemitoTransferencia).offset(skip).limit(limit)
-    result = await db.execute(stmt)
-    return result.scalars().all() 
 
-async def update_remito_transferencia(db: AsyncSession, remito_id: int, remito_update: RemitoTransferenciaCreateRequest):
-    
+async def get_all_remitos_transferencia(
+    db: AsyncSession, skip: int = 0, limit: int = 100
+):
+
+    stmt = (
+        select(RemitoTransferencia)
+        .options(
+            selectinload(RemitoTransferencia.cliente)
+            .selectinload(Cliente.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoTransferencia.producto)
+            .selectinload(Producto.proveedor)
+            .selectinload(Proveedor.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(RemitoTransferencia.sucursal)
+            .selectinload(Sucursal.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .offset(skip)
+        .limit(limit)
+    )
+    result = await db.execute(stmt)
+    return result.scalars().all()
+
+
+async def update_remito_transferencia(
+    db: AsyncSession, remito_id: int, remito_update: RemitoTransferenciaCreateRequest
+):
+
     stmt = select(RemitoTransferencia).where(RemitoTransferencia.id == remito_id)
     result = await db.execute(stmt)
     db_remito = result.scalar_one_or_none()
-    
+
     if db_remito:
-        
+
         for key, value in remito_update.model_dump(exclude_unset=True).items():
             setattr(db_remito, key, value)
         await db.commit()
@@ -48,12 +108,13 @@ async def update_remito_transferencia(db: AsyncSession, remito_id: int, remito_u
         return db_remito
     return None
 
+
 async def delete_remito_transferencia(db: AsyncSession, remito_id: int):
-    
+
     stmt = select(RemitoTransferencia).where(RemitoTransferencia.id == remito_id)
     result = await db.execute(stmt)
     db_remito = result.scalar_one_or_none()
-    
+
     if db_remito:
         await db.delete(db_remito)
         await db.commit()
