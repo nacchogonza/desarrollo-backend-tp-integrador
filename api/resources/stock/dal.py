f251fcbc197372dd8c9490c682c86e70f3593142
nacchogonza <nachomgonzalez93@gmail.com>
2025-06-26 00:50:55 -0300

    Se ajusta DAL  de Stock

--- api/resources/stock/dal.py
+++ api/resources/stock/dal.py
@@ -2,39 +2,43 @@ from fastapi import HTTPException, status
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import selectinload
-from .schemas import (
-    StockCreateRequest,
-    StockResponse
+from .schemas import StockCreateRequest, StockResponse
+from api.core.models import (
+    Stock,
+    Producto,
+    Proveedor,
+    Ciudad,
+    Provincia,
+    Deposito,
+    Sucursal,
 )
-from api.core.models import Stock, Producto
 
 
 async def obtener_stocks(db: AsyncSession):
     result = await db.execute(
         select(Stock)
-        .options(selectinload(Stock.producto))
-        .options(selectinload(Stock.deposito))
-        .options(selectinload(Stock.sucursal))
+        .options(
+            selectinload(Stock.producto)
+            .selectinload(Producto.proveedor)
+            .selectinload(Proveedor.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(Stock.deposito)
+            .selectinload(Deposito.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(Stock.sucursal)
+            .selectinload(Sucursal.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
     )
     return result.scalars().all()
 
-
-""" async def crear_stock(db: AsyncSession, stock: StockCreateRequest):
-    nuevo_stock = Stock(**stock.dict())
-    db.add(nuevo_stock)
-    await db.commit()
-    await db.refresh(nuevo_stock)
-    result = await db.execute(
-        select(Stock)
-        .options(selectinload(Stock.producto))
-        .options(selectinload(Stock.deposito))
-        .options(selectinload(Stock.sucursal))
-        .where(Stock.id == nuevo_stock.id)
-    )
-    stock_con_relacion = result.scalar_one()
-    return stock_con_relacion """
-    
-    
 async def crear_stock(db: AsyncSession, stock: StockCreateRequest):
     nuevo_stock = Stock(**stock.model_dump())
     db.add(nuevo_stock)
@@ -43,9 +47,25 @@ async def crear_stock(db: AsyncSession, stock: StockCreateRequest):
 
     result = await db.execute(
         select(Stock)
-        .options(selectinload(Stock.producto))
-        .options(selectinload(Stock.deposito))
-        .options(selectinload(Stock.sucursal))
+        .options(
+            selectinload(Stock.producto)
+            .selectinload(Producto.proveedor)
+            .selectinload(Proveedor.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(Stock.deposito)
+            .selectinload(Deposito.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
+        .options(
+            selectinload(Stock.sucursal)
+            .selectinload(Sucursal.ciudad)
+            .selectinload(Ciudad.provincia)
+            .selectinload(Provincia.pais)
+        )
         .where(Stock.id == nuevo_stock.id)
     )
     stock_con_relacion = result.scalar_one()
@@ -56,7 +76,7 @@ async def delete_stock(db: AsyncSession, stock_id: int):
     stmt = select(Stock).where(Stock.id == stock_id)
     result = await db.execute(stmt)
     db_remito = result.scalar_one_or_none()
-    
+
     if db_remito:
         await db.delete(db_remito)
         await db.commit()
